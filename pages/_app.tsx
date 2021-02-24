442bd181c10bd758fc30984f51ce7b887a56ecd6
grace <yurisine7@gmail.com>
2021-02-24 16:31:29 +0800

    adding components/chart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import { AppProps } from 'next/app'
 import AppContainer from '../components/app-container'
 import NavBar from '../components/navbar'
 import { Fragment } from 'react'
+import Chart from '../components/chart'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <NavBar></NavBar>
+      <Chart></Chart>
       <AppContainer>
         <Component {...pageProps} />
       </AppContainer>
