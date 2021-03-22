016b58103a67b94098720b24b635fe14a1cf690f
Kevin Tsai <b2778173@hotmail.com>
2021-03-22 23:34:57 +0800

    chrone : config extended

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
-module.exports = {
-  env: {
-    FLASK_API_BASE: "https://limitless-peak-52274.herokuapp.com"
-  }
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable prettier/prettier */
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_SERVER
+} = require("next/constants")
+
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            /* development only config options here */
+            env: {
+                FLASK_API_BASE: "http://localhost:5000"
+            }
+        }
+    }
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        return {
+            /* config options for all phases except development here */
+            env: {
+                FLASK_API_BASE: "https://limitless-peak-52274.herokuapp.com"
+            }
+        }
+    }
+    return defaultConfig
 }
