4bf0f973ab6c2580712c78389f37445ebd0500ec
Kevin Tsai <b2778173@hotmail.com>
2021-04-04 02:17:54 +0800

    fix: production build trigger

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_SERVER
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD
 } = require("next/constants")
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-      env: {
-        FLASK_API_BASE: "http://localhost:5000"
-      }
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            /* development only config options here */
+            env: {
+                FLASK_API_BASE: "http://localhost:5000"
+            }
+        }
     }
-  }
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return {
-      /* config options for all phases except development here */
-      env: {
-        FLASK_API_BASE: "https://limitless-peak-52274.herokuapp.com"
-      }
+    if (phase === PHASE_PRODUCTION_BUILD) {
+        return {
+            /* config options for all phases except development here */
+            env: {
+                FLASK_API_BASE: "https://limitless-peak-52274.herokuapp.com"
+            }
+        }
     }
-  }
-  return defaultConfig
+    return defaultConfig
 }
