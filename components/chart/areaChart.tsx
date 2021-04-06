bb89fb6a0f807a6f69051ff6405d8c91c312ef90
Kevin Tsai <b2778173@hotmail.com>
2021-04-06 13:35:01 +0800

    fix: ts error

--- components/chart/areaChart.tsx
+++ components/chart/areaChart.tsx
@@ -15,56 +15,58 @@ const canvasGradient = createVerticalLinearGradient([
   { stop: 0.7, color: hexToRGBA("#6fa4fc", 0.4) },
   { stop: 1, color: hexToRGBA("#4286f4", 0.8) }
 ])
-function AreaChart(props: {
-  data: any
-  type?: string
-  // eslint-disable-next-line react/require-default-props
-  width?: number
-  height?: number
-  ratio?: number
-  xExtents?: [Date, Date]
-}): any {
-  const { data, type, width, height, ratio, xExtents } = props
-  // console.log(data, type, width, height, ratio, xExtents)
-  useEffect(() => {})
-  return (
-    <ChartCanvas
-      ratio={ratio}
-      width={width}
-      height={height}
-      margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-      seriesName="MSFT"
-      data={data}
-      type={type}
-      xAccessor={(d: any) => d.date}
-      displayXAccessor={(d: any) => d.date}
-      xScale={scaleTime()}
-      zoomEvent={false}
-      xExtents={xExtents}>
-      {console.log("area chart render")}
-      <Chart id={0} yExtents={(d: any) => d.close}>
-        <defs>
-          <linearGradient id="MyGradient" x1="0" y1="100%" x2="0" y2="0%">
-            <stop offset="0%" stopColor="#b5d0ff" stopOpacity={0.2} />
-            <stop offset="70%" stopColor="#6fa4fc" stopOpacity={0.4} />
-            <stop offset="100%" stopColor="#4286f4" stopOpacity={0.8} />
-          </linearGradient>
-        </defs>
-        <XAxis axisAt="bottom" orient="bottom" ticks={6} />
-        <YAxis axisAt="left" orient="left" />
-        <AreaSeries
-          yAccessor={(d: any) => d.close}
-          fill="url(#MyGradient)"
-          strokeWidth={2}
-          interpolation={curveMonotoneX}
-          canvasGradient={canvasGradient}
-        />
-      </Chart>
-    </ChartCanvas>
-  )
-}
+const AreaChart = fitWidth(
+  (props: {
+    data: any
+    type?: string
+    // eslint-disable-next-line react/require-default-props
+    width?: number
+    height?: number
+    ratio?: number
+    xExtents?: [Date, Date]
+  }) => {
+    const { data, type, width, height, ratio, xExtents } = props
+    // console.log(data, type, width, height, ratio, xExtents)
+    useEffect(() => {})
+    return (
+      <ChartCanvas
+        ratio={ratio}
+        width={width}
+        height={height}
+        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+        seriesName="MSFT"
+        data={data}
+        type={type}
+        xAccessor={(d: any) => d.date}
+        displayXAccessor={(d: any) => d.date}
+        xScale={scaleTime()}
+        zoomEvent={false}
+        xExtents={xExtents}>
+        {console.log("area chart render")}
+        <Chart id={0} yExtents={(d: any) => d.close}>
+          <defs>
+            <linearGradient id="MyGradient" x1="0" y1="100%" x2="0" y2="0%">
+              <stop offset="0%" stopColor="#b5d0ff" stopOpacity={0.2} />
+              <stop offset="70%" stopColor="#6fa4fc" stopOpacity={0.4} />
+              <stop offset="100%" stopColor="#4286f4" stopOpacity={0.8} />
+            </linearGradient>
+          </defs>
+          <XAxis axisAt="bottom" orient="bottom" ticks={6} />
+          <YAxis axisAt="left" orient="left" />
+          <AreaSeries
+            yAccessor={(d: any) => d.close}
+            fill="url(#MyGradient)"
+            strokeWidth={2}
+            interpolation={curveMonotoneX}
+            canvasGradient={canvasGradient}
+          />
+        </Chart>
+      </ChartCanvas>
+    )
+  }
+)
 /* tslint:disable-next-line */
-AreaChart = fitWidth(AreaChart)
+// AreaChart = fitWidth(AreaChart)
 AreaChart.defaultProps = {
   type: "svg",
   // width: true,
